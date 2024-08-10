90a24256202e861c81c18bc4c2a8835a71b39deb
Ashish Kumar <ashishkumar901336@gmail.com>
2024-08-10 23:45:52 +0530

    react to resizing screen

--- scripts/main.js
+++ scripts/main.js
@@ -26,4 +26,10 @@ function animate() {
     renderer.render(scene, camera);
 }
 
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
 animate();
