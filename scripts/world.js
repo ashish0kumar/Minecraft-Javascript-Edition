34c086f4f3fa6479ff6ce4df7084eb3551d745a2
Ashish Kumar <ashishkumar901336@gmail.com>
2024-08-11 19:18:40 +0530

    world data model

--- scripts/world.js
+++ scripts/world.js
@@ -4,14 +4,51 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 
 export class World extends THREE.Group {
+    /**
+    * @type {{
+    *  id: number,
+    *  instanceId: number
+    * }[][][]}
+    */
+    data = [];
+
     constructor(size = { width: 64, height: 32 }) {
         super();
         this.size = size;
     }
 
     generate() {
+        this.generateTerrain();
+        this.generateMeshes();
+    }
+
+    /**
+     * Generates the world terrain data
+     */
+    generateTerrain() {
+        this.data = [];
+        for (let x = 0; x < this.size.width; x++) {
+            const slice = [];
+            for (let y = 0; y < this.size.height; y++) {
+                const row = [];
+                for (let z = 0; z < this.size.width; z++) {
+                    row.push({
+                        id: 1,
+                        instanceId: null
+                    });
+                }
+                slice.push(row);
+            }
+            this.data.push(slice);
+        }
+    }
+
+    /**
+     * Generates the 3D representation of the world from the world data
+     */
+    generateMeshes() {
         this.clear();
-        
+
         const maxCount = this.size.width * this.size.width * this.size.height;
         const mesh = new THREE.InstancedMesh(geometry, material, maxCount);
         mesh.count = 0;
@@ -20,12 +57,77 @@ export class World extends THREE.Group {
         for (let x = 0; x < this.size.width; x++) {
             for (let y = 0; y < this.size.height; y++) {
                 for (let z = 0; z < this.size.width; z++) {
-                    matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
-                    mesh.setMatrixAt(mesh.count++, matrix);
+                    const blockId = this.getBlock(x, y, z).id;
+                    const instanceId = mesh.count;
+
+                    if (blockId !== 0) {
+                        matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
+                        mesh.setMatrixAt(instanceId, matrix);
+                        this.setBlockInstanceId(x, y, z, instanceId);
+                        mesh.count++;
+                    }
                 }
             }
         }
 
         this.add(mesh);
+    }
+
+    /**
+     * Gets the block data at (x, y, z)
+     * @param {number} x
+     * @param {number} y
+     * @param {number} z
+     * @returns {{id: number, instanceId: number}}
+     */
+    getBlock(x, y, z) {
+        if (this.inBounds(x, y, z)) {
+            return this.data[x][y][z];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Sets the block id for the block at (x, y, z)
+     * @param {number} x
+     * @param {number} y
+     * @param {number} z
+     * @param {number} id
+     */
+    setBlockId(x, y, z, id) {
+        if (this.inBounds(x, y, z)) {
+            this.data[x][y][z].id = id;
+        }
+    }
+
+    /**
+     * Sets the block instance id for the block at (x, y, z)
+     * @param {number} x
+     * @param {number} y
+     * @param {number} z
+     * @param {number} instanceId
+     */
+    setBlockInstanceId(x, y, z, instanceId) {
+        if (this.inBounds(x, y, z)) {
+            this.data[x][y][z].instanceId = instanceId;
+        }
+    }
+
+    /**
+     * Checks if the (x, y, z) coordinates are within bounds
+     * @param {number} x
+     * @param {number} y
+     * @param {number} z
+     * @returns {boolean}
+     */
+    inBounds(x, y, z) {
+        if (x >= 0 && x < this.size.width &&
+            y >= 0 && y < this.size.height &&
+            z >= 0 && z < this.size.width) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
